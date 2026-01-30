6749fec2a4222bf720e492710d9480c00f23f8fa
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-01-30 16:39:44 +0300

    add util fn for setting text to titlecase

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function toTitleCase(str: string) {
+	return str
+		.toLowerCase()
+		.replace(/-/g, " ")
+		.replace(/\b\w/g, (l) => l.toUpperCase());
+}
