19a5f0508f94ffd6110c83ec577dd2d06b5c659c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-02-03 23:18:58 +0300

    get user info rather than member

--- src/features/profile/services/member.api.ts
+++ src/features/profile/services/member.api.ts
@@ -1,33 +1,16 @@
-import { redirect } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
 import { eq } from "drizzle-orm";
 import { db } from "@/db";
-import { members, users } from "@/drizzle/schema";
-import { NotFoundError } from "@/lib/error-handling/app-error";
+import { users } from "@/drizzle/schema";
 import { authMiddleware } from "@/middlewares/auth-middleware";
 
-export const getMemberInformation = createServerFn()
+export const getUserInformation = createServerFn()
 	.middleware([authMiddleware])
 	.handler(async ({ context }) => {
-		const memberId = await db.query.users.findFirst({
-			columns: { memberId: true },
+		const user = await db.query.users.findFirst({
+			columns: { name: true, image: true, email: true, contact: true },
 			where: eq(users.id, context.user.id),
 		});
 
-		if (!memberId || !memberId.memberId) throw redirect({ to: "/sign-in" });
-
-		const member = await db.query.members.findFirst({
-			columns: {
-				lastName: true,
-				firstName: true,
-				contact: true,
-				email: true,
-				image: true,
-			},
-			where: eq(members.id, memberId.memberId),
-		});
-
-		if (!member) throw new NotFoundError("Member info");
-
-		return member;
+		return user;
 	});
