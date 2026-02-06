4934c7e5a82a0b0b61ffb96f0f74357c40f0384c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-02-06 22:12:36 +0300

    schema & type for password change form

--- src/features/profile/services/schema.ts
+++ src/features/profile/services/schema.ts
@@ -9,4 +9,18 @@ export const editPersonalInfoSchema = z.object({
 	image: z.string().nullish(),
 });
 
+export const profileSearchValidate = z.object({
+	tab: z.enum(["personal", "security", "account"]).default("personal"),
+})
+
+export const changePasswordSchema = z.object({
+	currentPassword: z.string().min(1, "Current password is required").refine((value) => value.length >= 6, "Current password must be at least 6 characters long"),
+	newPassword: z.string().min(1, "New password is required").refine((value) => value.length >= 6, "New password must be at least 6 characters long"),
+	confirmPassword: z.string().min(1, "Confirm password is required").refine((value) => value.length >= 6, "Confirm password must be at least 6 characters long"),
+}).refine((data) => data.newPassword === data.confirmPassword, {
+	message: "Passwords do not match",
+	path: ["confirmPassword"],
+})
+
 export type EditPersonalInfoSchema = z.infer<typeof editPersonalInfoSchema>;
+export type ChangePasswordSchema = z.infer<typeof changePasswordSchema>;
