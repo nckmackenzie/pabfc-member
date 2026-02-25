3f4d5cda4d91da32d39416e148997070692d65cd
nckmackenzie <nicholas.mackenzie1@gmail.com>
2026-02-25 15:37:31 +0300

    add allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,48 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import { fileURLToPath, URL } from 'url'
+/** biome-ignore-all lint/style/noNonNullAssertion: <> */
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { fileURLToPath, URL } from "url";
+import { defineConfig, loadEnv } from "vite";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+const env = loadEnv("development", process.cwd(), "");
 
 const config = defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  plugins: [
-    devtools(),
-    nitro(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
-  ],
-})
+	server: {
+		allowedHosts:
+			env.NODE_ENV !== "production"
+				? [
+						"localhost",
+						"127.0.0.1",
+						"0.0.0.0",
+						"localhost:3000",
+						env.NGROK_DOMAIN!,
+					]
+				: true,
+	},
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	plugins: [
+		devtools(),
+		nitro(),
+		// this is the plugin that enables path aliases
+		viteTsConfigPaths({
+			projects: ["./tsconfig.json"],
+		}),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact({
+			babel: {
+				plugins: ["babel-plugin-react-compiler"],
+			},
+		}),
+	],
+});
 
-export default config
+export default config;
